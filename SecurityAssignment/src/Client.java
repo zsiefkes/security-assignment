66f8987284851a9fda076ba92d894332bf5daff8
Zachary Siefkes <zacharysiefkes@gmail.com>
2020-11-11 13:30:53 +1300

    Client can send task to server to write to db.

--- SecurityAssignment/src/Client.java
+++ SecurityAssignment/src/Client.java
@@ -4,58 +4,52 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 public class Client {
 
-//	private int clientId; // for later
+	private int clientId; // for later
 	private Socket socket;
+
+	private static ArrayList<Client> clientList = new ArrayList<Client>(); 
 	
 	public Client(String serverAddress, int port) throws UnknownHostException, IOException {
+		// open socket
 		this.socket = new Socket(serverAddress, port);
+		// assign unique client id and add Client to list of Clients
+		this.clientId = clientList.size() + 1;
+		clientList.add(this);
 	}
 	
-	public void writeTaskToDB(String task) throws IOException {
-		// okay so, we just use the code the task server was using to send the date to the client, but in the opposite directino now! yeah?
-		// um where's our socket coming from
-		// okay prob want to start a client with a socket, too. yeah?
-		// or do we open one each time we want to send a task ... :( ? 
-		// nah, seems to make more sense to open it and keep it open. okay
+	public void createTask(String task) throws IOException {
+		// open socket for writing to
 		PrintWriter writeToSocket = new PrintWriter(socket.getOutputStream(), true);
-//		System.out.println("A client request received at " + socket);
-//		String ts = new java.util.Date().toString();
-		writeToSocket.println(task);
-//		insertTimeStampIntoDB(ts); // client does not write to db
-//		socket.close(); // don't close the socket.... right?
 		
-		// and now wait for a response??? I dunno...
+		/* 
+		 * format the string in the agreed format: "[client ID]; [command string]; [task string]"
+		 * command strings are: CREATE to create a task; READ to retrieve client's list of tasks
+		 * eg. "1; CREATE; File tax return";
+		 * eg. "4; READ; "
+		 */
+		String toSend = clientId + "; CREATE; " + task;
+		writeToSocket.println(toSend);
+		
+		// and now wait for a response...
 		BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		String answer = input.readLine();
-//      //JOptionPane.showMessageDialog(null, answer);
 		System.out.println(answer);
 		System.exit(0);
 //		socket.close();
-		// where do we open the socket? in the um. constructor?
 	}
 	
-	// this here is basically the constructor.
-//	private void connectToServer(String serverAddress, int port) throws UnknownHostException, IOException {
-////        String serverAddress = "127.0.0.1";
-////        int port = 9090;
-//		this.socket = new Socket(serverAddress, port);
-//	}
+	public void readTasks() {
+		
+	}
+	
 	
     public static void main(String[] args) throws IOException {
-//        String serverAddress = "127.0.0.1";
-//        int port = 9090;
-//        Socket s = new Socket(serverAddress, port);
-//        BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
-//        String answer = input.readLine();
-//        //JOptionPane.showMessageDialog(null, answer);
-//        System.out.println(answer);
-//        System.exit(0);
-//        s.close();
     	// i want to see something from the client be read by the server
     	Client client = new Client("127.0.0.1", 9090);
-    	client.writeTaskToDB("Hello world!");
+    	client.createTask("Hello world!");
     }
 }
--- SecurityAssignment/src/TaskServerThread.java
+++ SecurityAssignment/src/TaskServerThread.java
@@ -5,6 +5,8 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class TaskServerThread extends Thread {
@@ -17,46 +19,125 @@ public class TaskServerThread extends Thread {
 	@Override
 	public void run() {
 		try {
-			// need code in here to actually um parse something incoming from client
-			// just do the opposite direction to what the client was doing accepting stuff from the server. input stream
-			// okay so the socket already exists, the um. the taskserver creates it and instantiates the thread with it. so don't need this line
-//	        Socket s = new Socket(serverAddress, port);
-			
-			// we want to read the input:
-	        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-	        String answer = input.readLine();
-	        //JOptionPane.showMessageDialog(null, answer);
-	        System.out.println(answer);
+			System.out.println("A client request received at " + socket);
+
+			// read input stream
+			BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	        String coded = input.readLine();
+
+	        // open output stream
+	        PrintWriter writeToSocket = new PrintWriter(socket.getOutputStream(), true);
+
+	        // read client id and parse incoming command
+	        String[] codedArray = coded.split("; ");
+	        int clientId = Integer.parseInt(codedArray[0]);
+
+	        if (codedArray[1].equals("CREATE")) {
+	        	
+	        	// parse task and create timestamp
+	        	String task = codedArray[2];
+	        	String timeStamp = new java.util.Date().toString();
+	        	
+	        	// write task to db
+	        	writeTaskToDB(clientId, task, timeStamp);
+	        	
+	        	// send message back to client
+	        	writeToSocket.println("Client " + clientId + " entered task: " + task + " at " + timeStamp);
+	        	
+	        } else if (codedArray[1].equals("READ")) {
+	        	
+	        	// run a query
+	        	String taskList = readTasksFromDB(clientId);
+
+	        	// send back to the client
+	        	writeToSocket.println("Client " + clientId + " tasks: " + taskList);
+	        	
+	        } else {
+	        	// send back to client faulty command message
+	        	writeToSocket.println("Command error; please provide CREATE or READ");
+	        }
+	        
+	        
+	        // don't forget we're going to have to run decryption step here too
+	        
+	        
+	        
+//	        System.out.println(answer);
 	        // need to parse the stuff.
+	        
+	        
 	        // not sure what this does here ....
 //	        System.exit(0);
 	        // don't close the socket yet! do we?
 //	        s.close();
 			
 			
-			PrintWriter writeToSocket = new PrintWriter(socket.getOutputStream(), true);
-			System.out.println("A client request received at " + socket);
-			String ts = new java.util.Date().toString();
-			writeToSocket.println(ts + "; " + answer);
-			insertTimeStampIntoDB(ts + "; "+ answer);
+//			PrintWriter writeToSocket = new PrintWriter(socket.getOutputStream(), true);
+//			System.out.println("A client request received at " + socket);
+//			String ts = new java.util.Date().toString();
+//			writeToSocket.println(ts + "; " + answer);
+//			writeTaskToDB(ts + "; "+ answer);
+			
 			socket.close();
 		} catch (IOException e) {
 			System.out.println("Error: " + e);
 			e.printStackTrace();
 		}
 	}
-
-	public void insertTimeStampIntoDB(String ts) {
+	
+	private Connection connectToDB() throws ClassNotFoundException, SQLException {
+		String databaseUser = "dizach";
+		String databaseUserPass = "123";
+		Class.forName("org.postgresql.Driver");
+		Connection connection = null;
+		String url = "jdbc:postgresql://localhost/testdb";
+		connection = DriverManager.getConnection(url, databaseUser, databaseUserPass);
+		return connection;
+	}
+	
+	public String readTasksFromDB(int clientId) {
 		try {
 			// System.out.println(Inet4Address.getLocalHost().getHostAddress());
-			String databaseUser = "dizach";
-			String databaseUserPass = "123";
-			Class.forName("org.postgresql.Driver");
-			Connection connection = null;
-			String url = "jdbc:postgresql://localhost/testdb";
-			connection = DriverManager.getConnection(url, databaseUser, databaseUserPass);
+			Connection connection = connectToDB();
 			Statement s = connection.createStatement();
-			s.executeUpdate("insert into timestamps values ('" + ts + "')");
+			String tableName = "tasks";
+			
+			String sqlCommand = "SELECT task FROM " + tableName + " WHERE client_id = " + clientId;
+//			s.executeUpdate("insert into tasks values ('" + ts + "')"); // note no need to end sql command string with semicolon 
+//			int result = s.executeUpdate(sqlCommand);
+			ResultSet results = s.executeQuery(sqlCommand);
+			String taskList = "";
+			// loop over result set and add to string to return
+			while (results.next()) {
+				taskList += results.getString("task") + "; ";
+			}
+			connection.close();
+			return taskList;
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Login Error: " + e.toString());
+			return "";
+		}
+		
+	}
+
+	public void writeTaskToDB(int clientId, String task, String timeStamp) {
+		// i guess the um. parsing of what's to be wrriten should happen in here? i confused. nah happen in run. or elsewhere.
+		try {
+			// System.out.println(Inet4Address.getLocalHost().getHostAddress());
+//			String databaseUser = "dizach";
+//			String databaseUserPass = "123";
+//			Class.forName("org.postgresql.Driver");
+//			Connection connection = null;
+//			String url = "jdbc:postgresql://localhost/testdb";
+//			connection = DriverManager.getConnection(url, databaseUser, databaseUserPass);
+			Connection connection = connectToDB();
+			Statement s = connection.createStatement();
+			String tableName = "tasks";
+			
+			String sqlCommand = "insert into " + tableName + " values (" + clientId + ", '" + task + "', '" + timeStamp + "')";
+			s.executeUpdate(sqlCommand);
+//			s.executeUpdate("insert into tasks values ('" + ts + "')"); // note no need to end sql command string with semicolon 
 			connection.close();
 		} catch (Exception e) {
 			e.printStackTrace();
